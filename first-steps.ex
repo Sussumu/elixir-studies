68ade5fd8a45d913f6b865f6e75ed9c7ac7e8f8a
Gabriel Kato <gabrielsussumu@outlook.com>
2019-12-14 18:42:09 -0300

    Set some functions as private

--- first-steps.ex
+++ first-steps.ex
@@ -1,12 +1,12 @@
 # all functions must be contained inside a module (it's like namespaces)
 defmodule Geometry do
     # every line has a return so there's no explicit return
-    def area(a, b) do
+    defp area(a, b) do
         a * b
     end
 
     # it's possible to define small functions in one line
-    def volume(area, height), do: area * height
+    defp volume(area, height), do: area * height
 
     # we can pipe functions sending the return as the first argument of the next func
     def run(a, b, h) do
